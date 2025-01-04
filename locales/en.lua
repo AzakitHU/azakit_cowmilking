7b57ad79bc6eabc129851d458a985e84ab980452
Azakit <155274165+AzakitHU@users.noreply.github.com>
2025-01-04 22:53:16 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -4,4 +4,7 @@ Locales["en"] = {
     reward = "You milk the cow.",
     failed = "You messed up!",
     process = "Milking a cow...",
+    process_bottle = "Pour into a bottle...",
+    need_more = "You need more plastic bottles!",
+    poured_milk = "You poured milk into the bottles.",
 }
